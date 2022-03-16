a4a05fc15036d952a3fe9236caee05b6680b8e0b
Retr0327 <philcoke35@gmail.com>
2022-03-16 20:52:26 +0800

    Update yamol_scraper

--- yamol/scraper/yamol_scraper.py
+++ yamol/scraper/yamol_scraper.py
@@ -1,10 +1,29 @@
-from ..downloader import YamolDownloader
 from dataclasses import dataclass
+from ..downloader import YamolDownloader
+from typing import Union, List, Generator
 
 
 @dataclass
 class YamolScraper:
+    """The YamolScraper extracts the option content from html tags downloaded by YamolDownloader"""
+
     id: int
 
-    def download_option_tags(self):
+    def download_option_tags(self) -> Union[str, List[str]]:
+        """The download_option_tags method downloads the html tags."""
         return YamolDownloader(self.id).download()
+
+    def clean_data(self, tag) -> str:
+        """The clean_data method cleans the html tags from the argument `tag`.
+
+        Args:
+            tag (BeautifulSoup): the BeautifulSoup object with option info.
+
+         Returns:
+            a str
+        """
+        return tag.find("span", class_="itemcontent").text
+
+    def extract_data(self) -> Generator[str]:
+        option_tags = self.download_option_tags()
+        return map(self.clean_data, option_tags)
