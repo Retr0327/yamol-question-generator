144922563897ccf502f058c756238d005513f6c9
Retr0327 <philcoke35@gmail.com>
2022-03-18 23:24:14 +0800

    Update docstring

--- yamol/latex_parser/components/title.py
+++ yamol/latex_parser/components/title.py
@@ -3,7 +3,9 @@ from pylatex.base_classes import Environment
 
 
 class Title(Environment):
-    """The Title object wraps LaTeX's center environment and creates a title."""
+    """
+    The Title object wraps LaTeX's center environment and creates a title.
+    """
 
     packages = [Package("center")]
     escape = False
