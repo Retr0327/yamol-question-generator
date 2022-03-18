e9d5e06dbef0d324a8a890de95cf5e7245045548
Retr0327 <philcoke35@gmail.com>
2022-03-18 23:02:33 +0800

    Add title

---
+++ yamol/latex_parser/components/title.py
@@ -0,0 +1,10 @@
+from pylatex.package import Package
+from pylatex.base_classes import Environment
+
+
+class Title(Environment):
+    """The Title object wraps LaTeX's center environment and creates a title."""
+
+    packages = [Package("center")]
+    escape = False
+    content_separator = "\n"
