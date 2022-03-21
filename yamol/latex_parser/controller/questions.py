4dab79159bc2e561dd2d8e2ec6e2cc7a0009ed84
Retr0327 <philcoke35@gmail.com>
2022-03-22 00:01:23 +0800

    Update question

--- yamol/latex_parser/controller/questions.py
+++ yamol/latex_parser/controller/questions.py
@@ -1,1 +1,24 @@
+from functools import reduce
+from pylatex import UnsafeCommand
 from ...question import YamolQuestionGenerator
+
+
+def create_latex_option_format(option_data: dict) -> str:
+    """The create_latex_option_format function creates the format of latex options.
+
+    Args:
+        option_data (dict): the data downloaded by `YamolQuestionGenerator`
+
+    Returns:
+        a str
+    """
+    question = option_data["question"]
+    option = option_data["option"]
+    option_string = reduce(lambda prev, current: f"{prev}" + f"}}{{{current}", option)
+
+    if option == "題組題":
+        return f"\\question {question}\n\n"
+
+    return (
+        f"\\question {question} \n\\choice{{{option_string}}}{{a}}\\vspace{{10pt}}\n\n"
+    )
