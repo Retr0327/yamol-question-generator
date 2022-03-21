9a35bcc59d477bed99709a00b778ba25de23e758
Retr0327 <philcoke35@gmail.com>
2022-03-21 20:46:34 +0800

    Add ChoicCommand

--- yamol/latex_parser/components/new_commands.py
+++ yamol/latex_parser/components/new_commands.py
@@ -1,6 +1,6 @@
 from typing import Union
 from itertools import chain
-from pylatex import UnsafeCommand
+from pylatex import UnsafeCommand, Command, NoEscape
 
 
 def show_all_answers() -> list[UnsafeCommand]:
@@ -180,3 +180,50 @@ class OptionIndentationCommand:
         tab_width = list(chain(*map(self.create_tab_width, tab_info)))
 
         return option_indentation + tab_width
+
+
+class ChoicCommand:
+
+    option_width = r"\ifdim\widthch<\widthchb\relax\setlength{\widthch}{\widthchb}\fi"
+
+    def set_to_width(self, option_type: str):
+        option_type = option_type.upper()
+        option_type_factory = {"A": 1, "B": 2, "C": 3, "D": 4}
+
+        return fr"\settowidth\widthchb{{{option_type}M.#{option_type_factory[option_type]}}}"
+
+    def set_each_option(self, number: str):
+        return fr"{number}{{#1}}{{#2}}{{#3}}{{#4}}{{#5}}"
+
+    def create(self):
+        extra_arguments = fr"""
+        {self.set_to_width('a')}
+
+        \setlength{{\widthch}}{{\widthcha}} 
+
+        {self.set_to_width('b')}
+
+        {self.option_width}
+        
+        {self.set_to_width('c')}
+
+        {self.option_width}
+
+        {self.set_to_width('d')}
+
+        {self.option_width}
+
+        \ifdim\widthch>\halftabwidth
+            {self.set_each_option('onech')}
+        \else\ifdim\widthch<\halftabwidth
+            {self.set_each_option('twoch')}
+        \ifdim\widthch>\fourthtabwidth
+            {self.set_each_option('fourch')}
+        \else
+
+        \fi\fi\fi        
+        """
+
+        return UnsafeCommand(
+            "newcommand", r"\choice", options=5, extra_arguments=extra_arguments
+        )
