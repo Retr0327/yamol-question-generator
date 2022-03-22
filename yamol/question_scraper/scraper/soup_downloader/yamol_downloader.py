cd89c7f7ef6363832974ff1370517fef3c4ad245
Retr0327 <philcoke35@gmail.com>
2022-03-22 16:04:23 +0800

    Remove comments

--- yamol/question_scraper/scraper/soup_downloader/yamol_downloader.py
+++ yamol/question_scraper/scraper/soup_downloader/yamol_downloader.py
@@ -11,14 +11,3 @@ class YamolDownloader:
     def download(self) -> BeautifulSoup:
         res = requests.get(self.url)
         return BeautifulSoup(res.text, "lxml")
-
-    # def download(self) -> Union[str, List[str]]:
-    #     res = requests.get(self.url)
-    #     soup = BeautifulSoup(res.text, "lxml")
-    #     question_tag = soup.find_all("div", class_="col-lg-12 reponse-card")
-    #     has_content = len(question_tag)
-
-    #     if not has_content:
-    #         return "no content"
-
-    #     return question_tag
