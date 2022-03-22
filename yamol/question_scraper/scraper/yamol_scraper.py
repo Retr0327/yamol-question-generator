9590909522e9398ac53466090b4b83592d77f51a
Retr0327 <philcoke35@gmail.com>
2022-03-22 16:02:21 +0800

    Update YamolScraper

--- yamol/question_scraper/scraper/yamol_scraper.py
+++ yamol/question_scraper/scraper/yamol_scraper.py
@@ -7,11 +7,20 @@ from .soup_downloader import YamolDownloader
 class YamolScraper:
     """The YamolScraper extracts the option content from html tags downloaded by YamolDownloader"""
 
-    id: int
+    test_id: int
+
+    def __post_init__(self) -> None:
+        self.soup = YamolDownloader(self.test_id).download()
 
     def download_option_tags(self) -> Union[str, List[str]]:
         """The download_option_tags method downloads the html tags."""
-        return YamolDownloader(self.id).download()
+        question_tag = self.soup.find_all("div", class_="col-lg-12 reponse-card")
+        has_content = len(question_tag)
+
+        if not has_content:
+            return "no content"
+
+        return question_tag
 
     def clean_data(self, tag) -> str:
         """The clean_data method cleans the html tags from the argument `tag`.
